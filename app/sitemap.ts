daffeec767ddaade65deb32b503b7db364126001
吳俊彥 <opelwu2002@gmail.com>
2026-09-16 12:56:34 +0800

    fix: 優化 sitemap.ts 結構

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,11 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://omni-astrology.vercel.app';
+  
   return [
     {
-      url: 'https://omni-astrology.vercel.app',
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1.0,
